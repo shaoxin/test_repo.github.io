shaoxin/test_repo.github.io

commit d003025a0879cf417d19279444f8868480f96907

add support for command 'getready'

ludo/js/protocol.js
@@ -168,7 +168,7 @@ LudoProtocol.prototype.parseProt_1_onConnect = function(senderID, msgObj) {
 
 LudoProtocol.prototype.parseProt_1_onPickup = function(senderID, msgObj) {
 	try {
-		var reply = new Object();
+		var reply = {};
 		reply.command = LudoProtocol.COMMAND.pickup + '_reply';
 
 		var request_user = game.getUserFromSenderID(senderID);
@@ -222,22 +222,79 @@ LudoProtocol.prototype.parseProt_1_onPickup = function(senderID, msgObj) {
 
 		reply.ret = true;
 		this.sendMsg(senderID, reply);
-
-		var broadcast_msg = new Object();
-		broadcast_msg.command =
-			LudoProtocol.COMMAND.pickup + '_notify';
-		var player_status = new Object();
-		player_status.color = color;
-		player_status.user_type = target_user_type;
-		player_status.isready = new_user.isready;
-		broadcast_msg.player_status = player_status;
-		this.broadcast(broadcast_msg);
 	} catch (err) {
+		reply = {};
+		reply.ret = false;
+		reply.error = err;
+		this.sendMsg(senderID, reply);
+		return;
+	}
+
+	var broadcastMsg = {};
+	broadcastMsg.command =
+		LudoProtocol.COMMAND.pickup + '_notify';
+	var player_status = {};
+	player_status.color = color;
+	player_status.user_type = target_user_type;
+	player_status.isready = new_user.isready;
+	broadcastMsg.player_status = player_status;
+	this.broadcast(broadcastMsg);
+
+	this.broadcastStartGame();
+};
+
+LudoProtocol.prototype.parseProt_1_onGetReady = function(senderID, msgObj) {
+	var reply = {};
+	reply.command = LudoProtocol.COMMAND.getready + '_reply';
+	try {
+		var request_user = game.getUserFromSenderID(senderID);
+		var orig_isready = request_user.isready;
+		request_user.isready = true;
+		reply.ret = true;
+
+		this.sendMsg(senderID, reply);
+
+		var broadcastMsg = {};
+		broadcastMsg.command = LudoProtocol.COMMAND.getready + '_notify';
+		broadcastMsg.colors = [];
+		for (p in request_user.players) {
+			reply.colors.push(p);
+		}
+		this.broadcast(broadcastMsg);
+
+		if (orig_isready == false)
+			this.broadcastStartGame();
+	} catch(err) {
+		reply = {};
 		reply.ret = false;
 		reply.error = err;
 		this.sendMsg(senderID, reply);
 	}
 };
+
+LudoProtocol.prototype.broadcastStartGame = function() {
+	i = 0;
+	while (p = game.players[i]) {
+		u = p.getUser();
+		if (u.type == User.TYPE.NOBODY) {
+			console.log('player ' + p.color +
+					' is not allocated a user do NOT start game');
+			return;
+		}
+		if (u.type == User.TYPE.HUMAN && u.isready == false) {
+			console.log('player ' + p.color + ' user ' + u.name +
+					' is not ready, do NOT start game');
+			return;
+		}
+		i++;
+	}
+	console.log('eveybody is ready, let us go!');
+	broadcastMsg = {};
+	broadcastMsg.command =
+		LudoProtocol.COMMAND.startgame + '_notify';
+	this.broadcast(broadcastMsg);
+};
+
 LudoProtocol.prototype.parseProt_1 = function(senderID, msgObj) {
 	try {
 		switch (msgObj.command) {
@@ -253,6 +310,7 @@ LudoProtocol.prototype.parseProt_1 = function(senderID, msgObj) {
 				break;
 
 			case LudoProtocol.COMMAND.getready:
+				this.parseProt_1_onGetReady(senderID, msgObj);
 				break;
 
 			case LudoProtocol.COMMAND.disready:

ludo/js/player.js
@@ -16,9 +16,17 @@ var Player = function (name, color, board) {
 };
 
 Player.prototype.setUser = function(user) {
+	if (this.user)
+		this.user.removePlayer(this);
+
 	var e = $('#li-' + this.color);
 	e.html('<div class="icon"></div>' + user.name);
+
     this.user = user;
+	user.addPlayer(this);
+
+	console.log("player-" + this.color +
+			" is occupied by user '" + user.name + "'");
 };
 
 Player.prototype.getUser = function() {

ludo/js/user.js
@@ -28,6 +28,7 @@ function User(type, isready, name, senderID) {
 	this.name = name;
 	this.senderID = senderID;
 	this.ishost = false;
+	this.players = {};
 };
 
 User.checkUserType = function (type) {
@@ -41,6 +42,13 @@ User.checkUserType = function (type) {
 
 User.prototype.setready = function() {
 	this.isready = true;
+};
+
+User.prototype.addPlayer = function(player) {
+	this.players[player.color] = player;
+};
+User.prototype.removePlayer = function(player) {
+	delete this.players[player.color];
 };
 
 global.User = User;

commit f3be101f6ea95c1ac03f7908403fd736c4b76def

fix 'pickup_reply' caused by commit d003025a

ludo/js/protocol.js
@@ -222,6 +222,18 @@ LudoProtocol.prototype.parseProt_1_onPickup = function(senderID, msgObj) {
 
 		reply.ret = true;
 		this.sendMsg(senderID, reply);
+
+		var broadcastMsg = {};
+		broadcastMsg.command =
+			LudoProtocol.COMMAND.pickup + '_notify';
+		var player_status = {};
+		player_status.color = msgObj.color;
+		player_status.user_type = target_user_type;
+		player_status.isready = new_user.isready;
+		broadcastMsg.player_status = player_status;
+		this.broadcast(broadcastMsg);
+
+		this.broadcastStartGame();
 	} catch (err) {
 		reply = {};
 		reply.ret = false;
@@ -229,18 +241,6 @@ LudoProtocol.prototype.parseProt_1_onPickup = function(senderID, msgObj) {
 		this.sendMsg(senderID, reply);
 		return;
 	}
-
-	var broadcastMsg = {};
-	broadcastMsg.command =
-		LudoProtocol.COMMAND.pickup + '_notify';
-	var player_status = {};
-	player_status.color = color;
-	player_status.user_type = target_user_type;
-	player_status.isready = new_user.isready;
-	broadcastMsg.player_status = player_status;
-	this.broadcast(broadcastMsg);
-
-	this.broadcastStartGame();
 };
 
 LudoProtocol.prototype.parseProt_1_onGetReady = function(senderID, msgObj) {