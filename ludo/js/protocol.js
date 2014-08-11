shaoxin/test_repo.github.io

commit dea6aea99acc0d18e79137069de1ed5e2a50779b

fix chromecast scenario: roll dice automatically if the current user is computer

ludo/js/protocol.js
@@ -313,6 +313,9 @@ LudoProtocol.prototype.broadcastStartGame = function() {
 		LudoProtocol.COMMAND.startgame + '_notify';
 	this.broadcast(broadcastMsg);
 	game.nextPlayer();
+	if (game.getCurrentPlayer().getUser().type === User.TYPE.COMPUTER)
+		game.board.dice.roll(game.rollDoneHandler,
+				game.rollDoneHandler_outofbusy);
 };
 
 LudoProtocol.prototype.parseProt_1 = function(senderID, msgObj) {

commit 6e03fe839f856ea13510bd8b8de5b13fe700398a

fix dice callback function scope error caused by commit dea6aea9

ludo/js/protocol.js
@@ -314,8 +314,8 @@ LudoProtocol.prototype.broadcastStartGame = function() {
 	this.broadcast(broadcastMsg);
 	game.nextPlayer();
 	if (game.getCurrentPlayer().getUser().type === User.TYPE.COMPUTER)
-		game.board.dice.roll(game.rollDoneHandler,
-				game.rollDoneHandler_outofbusy);
+		game.board.dice.roll(rollDoneHandler,
+				rollDoneHandler_outofbusy);
 };
 
 LudoProtocol.prototype.parseProt_1 = function(senderID, msgObj) {